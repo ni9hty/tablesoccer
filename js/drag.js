7d9169382095701cdb62a4f01a23ec9783b81f8a
ni9hty <ni9hty@yahoo.de>
2022-03-19 22:10:33 +0100

    drag and drop proof of concept

--- js/drag.js
+++ js/drag.js
@@ -1,3 +1,4 @@
+
 // target elements with the "draggable" class
 interact('.draggable')
   .draggable({
@@ -30,10 +31,28 @@ interact('.draggable')
     }
   })
 
+function dragMoveListener (event) {
+  var target = event.target
+  // keep the dragged position in the data-x/data-y attributes
+  var x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx
+  var y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy
+
+  // translate the element
+  target.style.transform = 'translate(' + x + 'px, ' + y + 'px)'
+
+  // update the posiion attributes
+  target.setAttribute('data-x', x)
+  target.setAttribute('data-y', y)
+}
+
+// this function is used later in the resizing and gesture demos
+window.dragMoveListener = dragMoveListener
+
+
 // enable draggables to be dropped into this
 interact('.dropzone').dropzone({
   // only accept elements matching this CSS selector
-  //accept: '#yes-drop',
+  //accept: '#gizn, #Nico',
   // Require a 75% element overlap for a drop to be possible
   overlap: 0.75,
 
@@ -50,13 +69,13 @@ interact('.dropzone').dropzone({
     // feedback the possibility of a drop
     dropzoneElement.classList.add('drop-target')
     draggableElement.classList.add('can-drop')
-    draggableElement.textContent = 'Dragged in'
+    //draggableElement.textContent = 'Dragged in'
   },
   ondragleave: function (event) {
     // remove the drop feedback style
     event.target.classList.remove('drop-target')
     event.relatedTarget.classList.remove('can-drop')
-    event.relatedTarget.textContent = 'Dragged out'
+    //event.relatedTarget.textContent = 'Dragged out'
   },
   ondrop: function (event) {
     event.relatedTarget.textContent = 'Dropped'
